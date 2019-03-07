d0dcbaa071e22491b7f0d583ea993680f274cd95
HoangNgocSonz <hoangson20bg@gmail.com>
2019-03-07 16:11:29 +0700

    initial commit

--- web 2/homework/app.py
+++ web 2/homework/app.py
@@ -7,7 +7,7 @@ bike=[]
 @app.route("/new_bike",methods = ["GET","POST"])
 def new_bike():
     if request.method == "GET":
-        return render_template("ex1.html")
+        return render_template("bike_form.html")
     elif request.method == "POST":
         form = request.form
         a=form["Model"]
